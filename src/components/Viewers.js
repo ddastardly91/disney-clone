65f4cade309cda8eefbc6a84816548abdb1b7be3
Aaron Latham <ALATHAM7891@GMAIL.COM>
2022-05-22 01:58:01 +0100

    Updated Viewers.js
    Fixed image alignment

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -55,6 +55,7 @@ const Wrap = styled.div`
     box-shadow: rgb(0 0 0 / 69%) 0 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
     transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
     overflow: hidden;
+    display: flex;
     
 
     img {
